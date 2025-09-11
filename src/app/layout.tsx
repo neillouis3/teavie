7ef496e75c153b975e494d802cf95d0e15643a0b
neillouis3 <neil03.castillon@gmail.com>
2025-09-11 20:51:03 -0230

    libraries initialization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+
+        <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Providers>
+            <div className="w-screen h-screen flex flex-col items-center">
+              
+              
+                {children}
+
+              
+            </div>
+
+          </Providers>
+        </body>
+      </html>
+
   );
 }
---
+++ src/app/providers.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import * as React from "react";
+import {HeroUIProvider} from "@heroui/system";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export function Providers({children}: ProvidersProps) {
+  const router = useRouter();
+
+  return (
+    <HeroUIProvider navigate={router.push}>
+      <NextThemesProvider attribute="class" defaultTheme="light" >{children}</NextThemesProvider>
+    </HeroUIProvider>
+  );
+}
