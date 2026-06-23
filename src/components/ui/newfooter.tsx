b7e3ef15b35948cbab91f44cd94e15d54d293c22
neillouis3 <neil03.castillon@gmail.com>
2026-06-23 06:09:20 -0230

    Update social handle to @neillouis3dev.
    Correct X links and credits on homepage, footer, and unavailable screen.

--- src/components/ui/newfooter.tsx
+++ src/components/ui/newfooter.tsx
@@ -5,7 +5,7 @@ export default function Footer () {
         <div className="bg-background w-full h-fit bottom-0 mt-auto justify-center items-center flex max-lg:hidden t-8  pb-16 pt-8 ">
             <div className="w-[75%] flex flex-row justify-between items-center">
                 <div className="flex items-center justify-center transition-colors">
-                    <p className="items-center text-sm text-foreground">Designed & built by @neillouisedev</p>
+                    <p className="items-center text-sm text-foreground">Designed & built by @neillouis3dev</p>
                 </div>
                 <div className="flex flex-row gap-4 items-center justify-center">
                     
